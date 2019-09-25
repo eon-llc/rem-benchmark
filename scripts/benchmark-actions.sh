487b1de61bfe425851011f748bfe48c70caa58a8
Serg Chernata <sergchernata@gmail.com>
2019-09-25 08:37:10 -0400

    simplified config

--- scripts/benchmark-actions.sh
+++ scripts/benchmark-actions.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-API_URL="http://127.0.0.1:8888"
-WALLET_NAME=""
-WALLET_PASS=""
-
 date
-remcli -u "$API_URL" wallet unlock -n "$WALLET_NAME" --password "$WALLET_PASS"
-remcli -u "$API_URL" push action rembenchmark cpu '[]' -p rembenchmark@active -f
-remcli -u "$API_URL" wallet stop
+remcli wallet unlock < /root/walletpass
+remcli push action rembenchmark cpu '[]' -p rembenchmark@active -f
+remcli wallet stop
 
 # add a delimeter for easy processing
 echo "--action--"
