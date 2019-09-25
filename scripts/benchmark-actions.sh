97c1dcdfc048eb31e273cfd81909183503595e6d
Serg Chernata <sergchernata@gmail.com>
2019-09-25 15:35:48 -0400

    format the timestamp in ISO-8601

--- scripts/benchmark-actions.sh
+++ scripts/benchmark-actions.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-date
+# timestamp in ISO-8601
+date -u +"%Y-%m-%dT%H:%M:%SZ"
+
+# perform benchmark
 remcli wallet unlock < /root/walletpass
 remcli push action rembenchmark cpu '[]' -p rembenchmark@active -f
 remcli wallet stop
