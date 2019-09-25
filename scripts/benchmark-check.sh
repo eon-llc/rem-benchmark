59d54b7b0726efcbf4a7074f204eeac350e6e487
Serg Chernata <sergchernata@gmail.com>
2019-09-25 15:36:07 -0400

    provide absolute path to python processor file

--- scripts/benchmark-check.sh
+++ scripts/benchmark-check.sh
@@ -2,11 +2,12 @@
 
 SCRIPT_DIR="/root/rem-benchmark/scripts"
 FILE_PATH="${SCRIPT_DIR}/benchmark-check.sh"
+PROCESSOR_PATH="${SCRIPT_DIR}/log_to_db.py"
 
 #---------------------------------
 # SCHEDULE THIS SCRIPT AS CRON
 #---------------------------------
-if ! crontab -l | grep -q ''
+if ! crontab -l | grep -q 'benchmark-check'
 then
   (crontab -l ; echo "* * * * * ${FILE_PATH}") | crontab -
 fi
@@ -19,4 +20,4 @@ pgrep benchmark-loop >/dev/null 2>&1 || $SCRIPT_DIR/benchmark-loop.sh &
 #---------------------------------
 # RUN LOG PROCESSOR
 #---------------------------------
-python log_to_db.py
+python $PROCESSOR_PATH
