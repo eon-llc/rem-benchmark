eb63cce1e62ff9385770752f23411550edcd0cab
Serg Chernata <sergchernata@gmail.com>
2019-09-27 07:58:30 -0400

    added crontab error logging

--- scripts/benchmark-check.sh
+++ scripts/benchmark-check.sh
@@ -3,19 +3,21 @@
 SCRIPT_DIR="/root/rem-benchmark/scripts"
 FILE_PATH="${SCRIPT_DIR}/benchmark-check.sh"
 PROCESSOR_PATH="${SCRIPT_DIR}/log_to_db.py"
+LOOP_PATH="${SCRIPT_DIR}/benchmark-loop.sh"
+LOG_PATH="${SCRIPT_DIR}/log.txt"
 
 #---------------------------------
 # SCHEDULE THIS SCRIPT AS CRON
 #---------------------------------
 if ! crontab -l | grep -q 'benchmark-check'
 then
-  (crontab -l ; echo "* * * * * ${FILE_PATH}") | crontab -
+  (crontab -l ; echo "* * * * * ${FILE_PATH}" >> $LOG_PATH 2>&1) | crontab -
 fi
 
 #---------------------------------
 # RESTART LOOP IF IT IS NOT RUNNING
 #---------------------------------
-pgrep benchmark-loop >/dev/null 2>&1 || $SCRIPT_DIR/benchmark-loop.sh &
+pgrep benchmark-loop >/dev/null 2>&1 || $LOOP_PATH &
 
 #---------------------------------
 # RUN LOG PROCESSOR
